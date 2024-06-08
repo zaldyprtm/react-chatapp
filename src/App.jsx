0b3d40edf600e45668992228548a78a47d829000
zaldyprtm <zaldyprtm@gmail.com>
2024-06-08 15:17:53 +0700

    'fix'

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,37 @@
-// src/App.js
 import React from 'react';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from './backend/Firebase';
 import Login from './components/Login';
 import Chat from './components/Chat';
 import Navbar from './components/layouts/Navbar';
-import  "./App.css";
-import { TypeAnimation } from 'react-type-animation';
+import "./App.css";
+
+import Hello from './components/layouts/Hello';
 
 const App = () => {
   const [user] = useAuthState(auth);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
+  // Menjalankan logika setelah user login
+  React.useEffect(() => {
+    if (user) {
+      setIsLoggedIn(true);
+    }
+  }, [user]);
 
   return (
     <>
-    <div>
-      <Navbar/>
-    </div>
-    <div className='mt-20 mx-auto w-80'>
-    <TypeAnimation
-    className='text-3xl text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500'
-      sequence={[
-        // Same substring at the start will only be typed out once, initially
-        'Silahkan Login Dahulu',
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
-        'untuk melanjutkan ke halaman chat',
-        1000,
-       
-      ]}
-      wrapper="span"
-      speed={50}
-      style={{ fontSize: '2em', display: 'inline-block' }}
-      repeat={Infinity}
-    />
-    </div>
-    <div className='-mt-56'>
-      {user ? <Chat /> : <Login />}
-    </div>
+      <div>
+        <Navbar />
+      </div>
+      
+      {/* Menampilkan Hello hanya jika user belum login */}
+      {!isLoggedIn && <Hello />}
+      
+      <div className='-mt-56'>
+        {/* Menampilkan komponen Chat jika user sudah login, dan Login jika belum */}
+        {user ? <Chat /> : <Login />}
+      </div>
     </>
   );
 };
---
+++ src/components/layouts/Hello.jsx
@@ -0,0 +1,26 @@
+import { TypeAnimation } from 'react-type-animation';
+
+const Hello = () => {
+  return (
+    <>
+      <div className="mt-20 mx-auto w-80">
+        <TypeAnimation
+          className="text-3xl text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500"
+          sequence={[
+            // Same substring at the start will only be typed out once, initially
+            "Silahkan Login Dahulu",
+            1000, // wait 1s before replacing "Mice" with "Hamsters"
+            "untuk melanjutkan ke halaman chat",
+            1000,
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{ fontSize: "2em", display: "inline-block" }}
+          repeat={Infinity}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Hello;
